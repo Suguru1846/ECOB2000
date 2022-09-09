389e75e89cd163e25794e5c0df4f8c3ab4ada760
Suguru <36467227+Suguru1846@users.noreply.github.com>
2022-09-08 22:22:44 -0400

    Update PP3.R

--- PP3.R
+++ PP3.R
@@ -15,6 +15,6 @@ np <- how_many_rolls*0.167
 
 #z score
 z <- (sum(if_come_up_6)-np)/sqrt(np*(1-0.167))
-pnorm(q=z, lower.tail=FALSE)
 
 #Use the positive Z score table to find values. If the Z value is out of the range between -1.64 to 1.64, the dice is unfair.
+pnorm(q=z, lower.tail=FALSE)
