66f68ea6dc7c78e02edbcee6edf0dd847bfa3874
Suguru <36467227+Suguru1846@users.noreply.github.com>
2022-09-08 22:21:11 -0400

    Add files via upload

---
+++ PP3.R
@@ -0,0 +1,20 @@
+PP3
+#H0 X = np
+#H1 X != np
+#CI = 95%
+
+#Rolling dice
+how_many_rolls <- 1000
+sim_rolls <- sample(1:6, how_many_rolls, replace = TRUE)
+
+#Number of 6 you get.
+if_come_up_6 <- as.numeric(sim_rolls == 6)
+
+#it is  probability when the dice is fair. Since 1/6 = 0.167
+np <- how_many_rolls*0.167
+
+#z score
+z <- (sum(if_come_up_6)-np)/sqrt(np*(1-0.167))
+pnorm(q=z, lower.tail=FALSE)
+
+#Use the positive Z score table to find values. If the Z value is out of rnge between -1.64 to 1.64, the dice is unfair.
